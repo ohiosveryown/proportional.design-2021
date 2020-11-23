6b568fe75ce7710b0ef970afc6e695463299275f
Matthew Pence <matt@ohiosveryown.co>
2020-11-23 01:23:55 +0000

    Added faux-loader

---
+++ components/Loader.vue
@@ -0,0 +1,42 @@
+<template lang="html">
+  <section>
+    <div class="track"/>
+  </section>
+</template>
+
+
+<style lang="scss" scoped>
+  @import '~/static/style/grid.scss';
+
+  section {
+    display: grid;
+    place-items: center;
+    position: fixed;
+    top: 0; left: 0; right: 0;
+    z-index: var(--zmax);
+    margin: 0 auto;
+    width: 40vw; height: 100vh;
+    overflow: hidden;
+    pointer-events: none;
+    @include breakpoint(mdl) { width: 24vw; }
+  }
+
+  .track {
+    width: 100%; height: .4rem;
+    background: var(--gravity);
+    animation: loader 1s cubic-bezier(.8,0,.16,1) forwards;
+    transform-origin: left;
+  }
+
+  @keyframes loader {
+    0% { transform: scaleX(0); }
+    50% { transform: scaleX(1); }
+    100% { transform: translateX(100%); }
+  }
+</style>
+
+<script>
+  export default {
+
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,53 @@
 <!-- layout -->
 <template>
-  <div class="width leave-target">
-    <Header class="skw"/>
-    <Hero
-      class="skw anim--enter"
-      :firstSrc="require('~/static/img/canyon-dresser-01.jpg')"
-      firstAlt="Canyon Dresser"
-      firstName="Canyon Dresser"
-      firstYear="2020"
-      firstMaterial="American Oak"
+  <div>
+    <Loader/>
+    <div class="width leave-target">
+      <Header class="skw"/>
+      <Hero
+        class="skw anim--enter"
+        :firstSrc="require('~/static/img/canyon-dresser-01.jpg')"
+        firstAlt="Canyon Dresser"
+        firstName="Canyon Dresser"
+        firstYear="2020"
+        firstMaterial="American Oak"
 
-      :secondSrc="require('~/static/img/mila-cabinet.jpg')"
-      secondAlt="Mila Cabinet"
-      secondName="Mila Cabinet"
-      secondYear="2020"
-      secondMaterial="Sapele"
-    />
-    <ul>
-      <List
-        class="skw"
-        v-for="project in projects"
-        :project="project"
-        :key="project.id"
+        :secondSrc="require('~/static/img/mila-cabinet.jpg')"
+        secondAlt="Mila Cabinet"
+        secondName="Mila Cabinet"
+        secondYear="2020"
+        secondMaterial="Sapele"
       />
-    </ul>
-    <Contact class="skw"/>
-    <Footer/>
+      <ul>
+        <List
+          class="skw"
+          v-for="project in projects"
+          :project="project"
+          :key="project.id"
+        />
+      </ul>
+      <Contact class="skw"/>
+      <Footer/>
+    </div>
   </div>
 </template>
 
 <!-- style -->
 <style lang="scss" scoped>
   @import '~/static/style/grid.scss';
+
+.loading-page {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(255, 255, 255, 0.8);
+  text-align: center;
+  padding-top: 200px;
+  font-size: 30px;
+  font-family: sans-serif;
+}
 
   ul {
     margin: 4rem 0 11.2rem;
@@ -41,6 +57,7 @@
 
 <!-- logic -->
 <script>
+  import Loader from '~/components/Loader'
   import Header from '~/components/Header'
   import Hero from '~/components/Hero'
   import List from '~/components/List'
@@ -52,10 +69,11 @@
     head: () => ({
       title: 'prop—home',
     }),
-    components: { Header, Hero, List, Contact, Footer, },
+    loading: '~/components/Loader.vue',
+    components: { Header, Hero, List, Contact, Footer, Loader },
     data() {
       return {
-        projects
+        projects,
       }
     },
     beforeDestroy() {
@@ -73,6 +91,7 @@
         skewY: 10,
         stagger: .05,
         duration: 1,
+        delay: 1,
         ease: Power2.easeInOut
       })
       gsap.from('.anim--delay', {
